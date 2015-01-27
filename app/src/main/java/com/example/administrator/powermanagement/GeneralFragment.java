8bc7b9b686eb8695ff9e4eaa5a4cb1610f130e2b
Gaoxiang.Zhang <seanjohnson@163.com>
2015-01-27 21:36:30 +0800

    A little modification to grid form

--- app/src/main/java/com/example/administrator/powermanagement/GeneralFragment.java
+++ app/src/main/java/com/example/administrator/powermanagement/GeneralFragment.java
@@ -1,6 +1,7 @@
 package com.example.administrator.powermanagement;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
